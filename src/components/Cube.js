dd503d348452ecac720bbd98a6e6998f13164e05
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-04 13:16:10 -0700

    displaying our individual cubes

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useReducer } from "react";
+import React, { Fragment, useEffect, useReducer } from "react";
 
 import SpinButtons from "./SpinButtons";
 import { cubeReducer, initialCubeState } from "../cubeReducer";
@@ -6,10 +6,25 @@ import { cubeReducer, initialCubeState } from "../cubeReducer";
 const Cube = () => {
   const [cubeState, cubeDispatch] = useReducer(cubeReducer, initialCubeState);
 
+  useEffect(() => {
+    console.log(cubeState.cube[cubeState.currentSide]);
+  }, []);
+
   return (
     <div className="cube">
       <SpinButtons cubeDispatch={cubeDispatch} />
       Cube<span style={{ fontSize: "3rem" }}>{cubeState.currentSide}</span>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          rowGap: "4rem",
+        }}
+      >
+        {cubeState.cube[cubeState.currentSide].map((num, index) => {
+          return <div key={index}>{num}</div>;
+        })}
+      </div>
     </div>
   );
 };
