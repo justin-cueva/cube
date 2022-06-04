de9a90898a29bc9297ef47636aea8ead25f72760
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-04 15:00:53 -0700

    spinning our bottom row to the left

---
+++ src/components/MixButtons.js
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+
+const MixButtons = ({ cubeDispatch }) => {
+  return (
+    <Fragment>
+      {/* <button
+        type="button"
+        className="mix-btn mix-btn--top-left"
+        onClick={() => cubeDispatch({ type: "", payload: { currentSide: 1 } })}
+      >
+        mix
+      </button>
+      <button
+        type="button"
+        className="mix-btn mix-btn--top-right "
+        onClick={() => cubeDispatch({ type: "", payload: { currentSide: 1 } })}
+      >
+        mix
+      </button> */}
+      <button
+        type="button"
+        className="mix-btn mix-btn--bottom-left "
+        onClick={() =>
+          cubeDispatch({
+            type: "ROTATE_BOTTOM_TO_LEFT",
+            payload: { button: "LEFT" },
+          })
+        }
+      >
+        mix
+      </button>
+      {/* <button
+        type="button"
+        className="mix-btn mix-btn--bottom-right"
+        onClick={() =>
+          cubeDispatch({ type: "ROTATE_BOTTOM", payload: { button: "RIGHT" } })
+        }
+      >
+        mix
+      </button> */}
+    </Fragment>
+  );
+};
+
+export default MixButtons;
--- src/cubeReducer.js
+++ src/cubeReducer.js
@@ -17,6 +17,47 @@ export const initialCubeState = {
 
 export const cubeReducer = (state = initialCubeState, action) => {
   switch (action.type) {
+    case "ROTATE_BOTTOM_TO_LEFT":
+      const newCurrentSide = [...state.cube[state.currentSide]].map(
+        (item, index) => {
+          if (index === 6 || index === 7 || index === 8) {
+            return state.cube[state.right][index];
+          } else {
+            return item;
+          }
+        }
+      );
+      const newRightSide = [...state.cube[state.right]].map((item, index) => {
+        if (index === 6 || index === 7 || index === 8) {
+          return state.cube[state.back][index];
+        } else {
+          return item;
+        }
+      });
+      const newBackSide = [...state.cube[state.back]].map((item, index) => {
+        if (index === 6 || index === 7 || index === 8) {
+          return state.cube[state.left][index];
+        } else {
+          return item;
+        }
+      });
+      const newLeftSide = [...state.cube[state.left]].map((item, index) => {
+        if (index === 6 || index === 7 || index === 8) {
+          return state.cube[state.currentSide][index];
+        } else {
+          return item;
+        }
+      });
+      return {
+        ...state,
+        cube: {
+          ...state.cube,
+          [state.currentSide]: newCurrentSide,
+          [state.right]: newRightSide,
+          [state.back]: newBackSide,
+          [state.left]: newLeftSide,
+        },
+      };
     case "VIEW_TOP":
       return {
         ...state,
