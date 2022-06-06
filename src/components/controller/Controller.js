2c56284479663da84c5a744fc25bd05ba9ff6a73
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-06 10:20:38 -0700

    mapping over buttons

--- src/components/controller/Controller.js
+++ src/components/controller/Controller.js
@@ -1,54 +1,31 @@
 import React from "react";
 import "../../styles/controller/controller.css";
 
+const buttons = [
+  { view: "Front", number: 0 },
+  { view: "Back", number: 5 },
+  { view: "Left", number: 2 },
+  { view: "Right", number: 4 },
+  { view: "Top", number: 1 },
+  { view: "Bottom", number: 3 },
+];
+
 const Controller = ({ cubeDispatch }) => {
   return (
     <div className="controller-container">
-      <h2>Controller</h2>
-      <div>x</div>
-      <span>Viewing Front</span>
-      <button
-        type="button"
-        className="view-btn x-0"
-        onClick={() => cubeDispatch({ type: "VIEW_FRONT" })}
-      >
-        Front
-      </button>
-      <button
-        type="button"
-        className="view-btn x-5 "
-        onClick={() => cubeDispatch({ type: "VIEW_BACK" })}
-      >
-        Back
-      </button>
-      <button
-        type="button"
-        className="view-btn x-2"
-        onClick={() => cubeDispatch({ type: "VIEW_LEFT" })}
-      >
-        Left
-      </button>
-      <button
-        type="button"
-        className="view-btn x-4"
-        onClick={() => cubeDispatch({ type: "VIEW_RIGHT" })}
-      >
-        Right
-      </button>
-      <button
-        type="button"
-        className="view-btn x-1"
-        onClick={() => cubeDispatch({ type: "VIEW_TOP" })}
-      >
-        Top
-      </button>
-      <button
-        type="button"
-        className="view-btn x-3"
-        onClick={() => cubeDispatch({ type: "VIEW_BOTTOM" })}
-      >
-        Bottom
-      </button>
+      <h2 style={{ fontSize: "2rem" }}>Controller</h2>
+      {buttons.map(({ view, number }, index) => {
+        return (
+          <button
+            key={index}
+            type="button"
+            className={`view-btn x-${number}`}
+            onClick={() => cubeDispatch({ type: `VIEW_${view.toUpperCase()}` })}
+          >
+            {view}
+          </button>
+        );
+      })}
     </div>
   );
 };
